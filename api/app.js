42e842d23a8dba32ba17a8044b02fcb8681ba089
Michael Perry <pusheadmetal@gmail.com>
2019-08-15 15:44:35 -0400

    MERN Active

--- api/app.js
+++ api/app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
+var testDBRouter = require("./routes/testDB");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/testAPI");
 
 var app = express();
 
@@ -13,15 +16,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/testDB", testDBRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use("/testAPI", testAPIRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
